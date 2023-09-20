c76999bfa1be81dcafd14e879945fa602bf14cce
psau <psau@umich.edu>
2023-09-20 11:28:05 -0400

    Wrote area function

--- discussion_4.py
+++ discussion_4.py
@@ -37,8 +37,10 @@ class Rectangle():
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
-
-
+    def area(self):
+        if self.verify_input():
+            return self.width * self.height
+        return "Invalid input"
 
     # Create the "perimeter" method
     #
