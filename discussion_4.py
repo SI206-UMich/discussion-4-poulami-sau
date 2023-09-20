29717c7a037a560a1adf651489dd26bda7a01e8b
psau <psau@umich.edu>
2023-09-20 11:26:13 -0400

    Wrote verify_input function

--- discussion_4.py
+++ discussion_4.py
@@ -26,8 +26,10 @@ class Rectangle():
     #       False otherwise
 
     # YOUR CODE HERE
-
-
+    def verify_input(self):
+        if self.width > 0 and self.height > 0:
+            return True
+        return False
 
     # Create the "area" method
     #
